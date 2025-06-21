ed1bf64469b0e32ec50957e2782a4f00e14c2fd1
kateacraig <kate.a.craig@gmail.com>
2025-06-21 16:39:20 -0400

    Updated JS

--- script/index.js
+++ script/index.js
@@ -5,3 +5,11 @@ function toggleMobileMenu(menu) {
 function toggleSidebar(menu) {
   menu.classList.slideToggle("open");
 }
+
+$(document).ready(function () {
+  // Select the element that will trigger the toggle (e.g., a menu button)
+  $("#hamburger").click(function () {
+    // Select the navigation menu element to be toggled
+    $(".sidebar").toggleClass("active"); // 'fast', 'slow', or a number in milliseconds
+  });
+});
