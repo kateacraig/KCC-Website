1c4ca3081484fb73d73e4c82ac291e106cee88c7
kateacraig <kate.a.craig@gmail.com>
2025-06-20 21:26:11 -0400

    Updated HTML, CSS, and JS to hide sidebar when not in use.

--- script/index.js
+++ script/index.js
@@ -1,3 +1,14 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
+function changeMenu() {
+  if (body.classList.contains("hidden")) {
+    body.classList.remove("hidden");
+  } else {
+    body.classList.add("hidden");
+  }
+}
+
+let hamburgerButton = document.querySelector(".hamburger-menu");
+hamburgerButton.addEventListener("click", changeMenu);
